33c04350faf37c70f38171dc59a8364d0279b4e4
Abhay Pandey <139734412+abhaypandey01@users.noreply.github.com>
2025-07-09 08:53:05 +0530

    Update question1.java

--- question1.java
+++ question1.java
@@ -3,7 +3,7 @@ class Main {
         int length, breadth, area, parameter;
         length = 70;
         parameter = 230;
-        breadth = parameter - length;
+        breadth = parameter/2 - length;
         area = length * breadth;
         System.out.println("Breadth ="+breadth+"cm");
         System.out.println("Area ="+area+"cm2");
