f4f2289f8176b899c42075f5bd9932a26c6f0bfd
Abhay Pandey <abhaymp09@gmail.com>
2025-07-09 07:54:27 +0530

    27 done

---
+++ question27.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String args[]){
+        int length = 20, breadth = 15, costpm = 5, diningArea, totalCost;
+        diningArea = length*breadth;
+        totalCost = costpm * diningArea;
+        System.out.println("The total cost for dining room will be "+totalCost+"$");
+    }
+}
